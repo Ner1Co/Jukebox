46b6da751e8ac6cc205be35bfa5389c8622b5398
Ner1Co <ner.saada@gmail.com>
2016-08-27 19:22:32 +0300

    Added song model and Microsoft API for parsing comments

--- common/models/user.js
+++ common/models/user.js
@@ -1,3 +1,55 @@
 module.exports = function(user) {
+    //data : {spotId, song : {link, api}, comment}
+    user.newSuggestion = function (id, data, callback){
+        var Song = user.app.models.Song;
+        var Suggestion = user.app.models.Suggestion;
+        var Vote = user.app.models.Vote;
 
+        function createSuggestion(song) {
+            Suggestion.create({userId: id, songId: song.id, spotId: data.spotId}, (err, newSuggestion) => {
+                Vote.create({score: 1, date: new Date(), comment: data.comment, suggestionId: newSuggestion.id,
+                    userId: id, spotId: data.spotId}, (err, newVote) => {
+                    newSuggestion.vote = newVote;
+                    callback(null, newSuggestion);
+                });
+            });
+        }
+
+
+
+        Song.findOne({where: {and :[{api: data.song.api}, {link: data.song.link}]}}, (err ,foundSong) => {
+            if(foundSong) {
+                createSuggestion(foundSong);
+            } else {
+                Song.create(data.song, (err, newSong) => {
+                    createSuggestion(newSong);
+                })
+            }
+        });
+    };
+
+    user.remoteMethod(
+        'newSuggestion',
+        {
+            accepts: [{arg: 'id', type: 'string'}, {arg: 'data', type: 'json', http: { source: 'body' }}],
+            http: {path: '/:id/newSuggestion', verb: 'post'},
+            returns: {arg: 'result', type: 'string', root: true}
+        }
+    );
 };
+
+
+/**
+ * Examples
+ *
+ *
+ {
+   "spotId": "1",
+   "song": {
+     "link": "https://www.youtube.com/watch?v=T2zazSUlxzU",
+     "api": "youtube"
+   },
+   "comment": "new comment"
+ }
+ *
+ */
--- common/models/vote.js
+++ common/models/vote.js
@@ -1,3 +1,41 @@
+var request = require('request');
+
 module.exports = function(Vote) {
 
+    function getKeyPhrases(string, callback){
+        var post_data = {
+            documents: [
+                {
+                    language: "en",
+                    id: "1",
+                    text: string
+                }
+            ]
+        };
+
+        var post_options = {
+            url: 'https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/keyPhrases',
+            method: 'POST',
+            json: post_data,
+            headers: {
+                'Content-Type': 'application/json',
+                'Ocp-Apim-Subscription-Key': '4bf90a46c747404094b48697a4fa1d10'
+            }
+        };
+
+        request(post_options, function(err, res, body) {
+            callback(err, body.documents[0].keyPhrases);
+        });
+    }
+
+    Vote.observe('before save', function computePercentage(ctx, next) {
+        if (ctx.instance && ctx.instance.comment) {
+            getKeyPhrases(ctx.instance.comment, (err, keyPhrases) => {
+                ctx.instance.tags = keyPhrases;
+                next();
+            });
+        } else {
+            next();
+        }
+    });
 };
