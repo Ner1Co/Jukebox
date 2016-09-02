da749443257132f15192a9757016568e9561d4d5
Ner1Co <ner.saada@gmail.com>
2016-09-02 18:19:48 +0300

    added youtube-api

--- common/models/user.js
+++ common/models/user.js
@@ -1,3 +1,6 @@
+var YouTube = require('youtube-node');
+var youTube = new YouTube();
+
 module.exports = function(user) {
     //data : {spotId, song : {link, api}, comment}
     user.newSuggestion = function (id, data, callback){
@@ -31,9 +34,49 @@ module.exports = function(user) {
     user.remoteMethod(
         'newSuggestion',
         {
-            accepts: [{arg: 'id', type: 'string'}, {arg: 'data', type: 'json', http: { source: 'body' }}],
+            accepts: [{arg: 'id', type: 'string'}, {arg: 'data', type: 'object', http: { source: 'body' }}],
             http: {path: '/:id/newSuggestion', verb: 'post'},
             returns: {arg: 'result', type: 'string', root: true}
+        }
+    );
+
+    user.getNextSong = function (id, spotId, callback) {
+        var Song = user.app.models.Song;
+        var Suggestion = user.app.models.Suggestion;
+        var Vote = user.app.models.Vote;
+        var Spot = user.app.models.Spot;
+
+        Spot.findById({id: spotId}, {include: [{suggestions: "songs"}, "playedSongs"]}, (err, spot) => {
+
+        });
+        // Suggestion.find({include: "song"},(err, suggestions) => {
+        //
+        // });
+
+
+
+        youTube.setKey('AIzaSyB1OOSpTREs85WUMvIgJvLTZKye4BVsoFU');
+
+        youTube.getRating('HcwTxRuq-uk', function(error, result) {
+            if (error) {
+                callback(error, null);
+               // console.log(error);
+            }
+            else {
+                callback(null, result);
+               // console.log(JSON.stringify(result, null, 2));
+            }
+        });
+
+
+    };
+
+    user.remoteMethod(
+        'getNextSong',
+        {
+            accepts: [{arg: 'id', type: 'string'}, {arg: 'spotId', type: 'string'}],
+            http: {path: '/:id/getNextSong', verb: 'get'},
+            returns: {arg: 'result', type: 'object', root: true}
         }
     );
 };
