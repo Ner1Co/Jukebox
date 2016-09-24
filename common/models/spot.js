886d3de474bc91b40f113e4f6b14e027d77f2866
Boris Talesnik <boris@lightricks.com>
2016-09-24 17:20:29 +0200

    spot.js: add extendedSuggestions. (#2)

--- common/models/spot.js
+++ common/models/spot.js
@@ -1,3 +1,35 @@
-module.exports = function(Spot) {
+module.exports = function(spot) {
 
+    spot.extendedSuggestions = function (id, callback) {
+
+        var Suggestion = spot.app.models.Suggestion;
+        var filter =
+        {
+            where: {
+                spotId: id
+            },
+            include:[
+                {
+                    relation:"song"
+                },
+            ]
+        };
+
+        Suggestion.find(filter, (err ,suggestions) => {
+            if (err) {
+                callback(err, null);
+                return;
+            }
+            callback(null, suggestions);
+        })
+    };
+
+    spot.remoteMethod(
+        'extendedSuggestions',
+        {
+            accepts: [{arg: 'id', type: 'string'}],
+            http: {path: '/:id/extendedSuggestions', verb: 'get'},
+            returns: {arg: 'result', type: 'object', root: true}
+        }
+    );
 };
